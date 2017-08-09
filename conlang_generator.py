bddb793f866ca66f842ac2f5604ac64f537ba127
cawilliams <cawilliams@users.noreply.github.com>
2017-08-10 01:10:28 +0300

    Moved globals to class, refactoring

--- conlang_generator.py
+++ conlang_generator.py
@@ -1,132 +1,126 @@
 #!/usr/bin/python3
-import os
-import sys
 import json
 import random
 
-words = []
-phonemes = {}
-exceptions = []
-filename = 'example.json'
-
 running = True
 
-def generate_words(sample, phonemes, num=10):
-    global words
-    gen = True
-    r = random.random()
-    word = ""
-    i = 0
-    while i < int(num):
-        for j in range(0, len(sample)):
-            if sample[j] == '(':
-                gen = (random.randint(0,1) == 0)
-            elif sample[j] == ')':
-                gen = True
-            else:
-                for n, phtype in enumerate(phonemes.keys()):
-                    if gen and phtype == sample[j]:
-                        k = random.choice(phonemes[phtype])
-                        word += k
-        if (not word in words) and (not isExceptional(word)):
-            words.append(word)
+class Phonetics:
+    filename = 'example.json'
+    phonemes = {}
+    exceptions = []
+    words = []
 
-        i += 1
+    def __init__(self):
+        self.phonemes = {}
+        self.exceptions = []
+
+    def load(self):
+        with open(self.filename, 'r') as infile:
+            data = json.load(infile)
+            self.phonemes = data["phonemes"]
+            self.exceptions = data["exceptions"]
+
+    def save(self):
+        data = {
+            "phonemes": self.phonemes,
+            "exceptions": self.exceptions
+        }
+        with open(self.filename, 'w') as outfile:
+            json.dump(data, outfile)
+
+    def is_word_exceptional(self, word):
+        for ex in self.exceptions:
+            if ex in word:
+                return True
+        return False
+
+    def generate_words(self, sample, num=10):
+        self.words = []
+        gen = True
         word = ""
+        i = 0
+        while i < int(num):
+            for j in range(0, len(sample)):
+                if sample[j] == '(':
+                    gen = random.randint(0, 1)
+                elif sample[j] == ')':
+                    gen = True
+                else:
+                    if gen:
+                        for phtype in self.phonemes.keys():
+                            if phtype == sample[j]:
+                                k = random.choice(self.phonemes[phtype])
+                                word += k
 
-def isExceptional(word):
-    global exceptions
-    for i in range(0, len(exceptions)):
-        if exceptions[i] in word:
-            return True
-    return False
+            if not (word in self.words or self.is_word_exceptional(word)):
+                self.words.append(word)
+                i += 1
+            word = ""
+
+        return self.words
 
 
-
-def command_exec(cmd):
-    global words, phonemes, filename, exceptions
+def command_exec(phonetics, cmd):
     params = cmd.split()
     if params == []:
-        print("Phonemes:\n")
-        print(phonemes)
-        print("Forbidden clusters:\n")
-        print(exceptions)
+        print("Phonemes:")
+        print(phonetics.phonemes)
+        print("Forbidden clusters:")
+        print(phonetics.exceptions)
 
     elif params[0] == "exit":
-        running = False
+        return (False, phonetics)
 
     elif params[0] == "gen":
-        words = []
         if len(params) == 2:
-            generate_words(params[1], phonemes)
+            words = phonetics.generate_words(params[1])
         elif len(params) == 3:
-            generate_words(params[1], phonemes, params[2])
+            words = phonetics.generate_words(params[1], params[2])
         else:
             print("! Error, can't generate words")
 
-        for i,w in enumerate(words):
-            if i==0 or i%10:
+        for i, w in enumerate(words):
+            if i == 0 or i%10:
                 print(w+" ", end='')
             else:
                 print(w+"\n", end='')
         print()
 
     elif params[0] == "set":
-        #pars = 0
-        
+
         if params[1] == "ph" and len(params) == 4:
-            phonemes[params[2]] = params[3]
+            phonetics.phonemes[params[2]] = params[3]
+
         elif params[1] == "ex" and len(params) > 2:
-            i = 2
-            while i < len(params):
-                if not params[i] in exceptions:
-                    exceptions.append(params[i])
+            for ex in params[2:]:
+                if not ex in phonetics.exceptions:
+                    phonetics.exceptions.append(ex)
                 else:
-                    exceptions.remove(params[i])
-                i += 1
+                    phonetics.exceptions.remove(ex)
         else:
             print("! Error, can't execute a command")
-
-        
 
     elif params[0] == "savewords":
         if len(params) == 2:
             sw = open(params[1]+".txt", 'w')
-            for i,w in enumerate(words):
-                if i==0 or i%10:
+            for i, w in enumerate(phonetics.words):
+                if i == 0 or i%10:
                     sw.write(w+' ')
                 else:
                     sw.write(w+'\n')
 
     elif params[0] == "reset":
-        phonemes = {}
-        exceptions = []
+        phonetics = Phonetics()
 
     elif params[0] == "save":
-        if (len(params) > 2):
-            if params[1] == "ph":
-                filename = params[2]
-                with open(filename, 'w') as outfile_ph:
-                    json.dump(phonemes, outfile_ph)
-            elif params[1] == "ex":
-                filename = params[2]
-                with open(filename, 'w') as outfile_ex:
-                    json.dump(exceptions, outfile_ex)
-        else:
-            print("! Error, can't save a file")
+        if len(params) > 1:
+            phonetics.filename = params[1]
+        phonetics.save()
 
     elif params[0] == "load":
-        if (len(params)>2):
-            if params[1] == "ph":
-                filename = params[2]
-                with open(filename, 'r') as data_file_ph:    
-                    phonemes = json.load(data_file_ph)
-            elif params[1] == "ex":
-                filename = params[2]
-                with open(filename, 'r') as data_file_ex:    
-                    exceptions = json.load(data_file_ex)
-        else:
-            print("! Error, can't load a file")
+        if len(params) > 1:
+            phonetics.filename = params[1]
+        phonetics.load()
 
     elif params[0] == "help":
         print("set ph <typeOfPhoneme> <phonemes> - set class of phonemes typed with a single string;")
@@ -140,9 +134,12 @@ def command_exec(cmd):
         print("savewords <filename> - save wordlist to file;")
         print("exit - close program")
     
+    return (True, phonetics)
 
 
 print("This is a word generator for your conlang :3\nType 'help' for additional info\n")
+phon = Phonetics()
+
 while running:
     command = input(">> ")
-    command_exec(command)
+    running, phon = command_exec(phon, command)
